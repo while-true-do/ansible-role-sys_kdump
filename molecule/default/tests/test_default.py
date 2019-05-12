7d540c226e927c21ee77d4a6aae9e12caea18e7e
daniel-wtd <daniel@while-true-do.io>
2019-05-12 09:20:28 +0200

    feat: fedora 30 support

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,5 +7,22 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_kdump_pkg(host):
-    pkg = host.package('kexec-tools')
-    assert pkg.is_installed
+    vrt = host.ansible("setup")["ansible_facts"]["ansible_virtualization_type"]
+    if vrt != 'docker':
+        pkg = host.package('kexec-tools')
+        assert pkg.is_installed
+
+
+def test_kdump_conf(host):
+    vrt = host.ansible("setup")["ansible_facts"]["ansible_virtualization_type"]
+    if vrt != 'docker':
+        file = host.file('/etc/kdump.conf')
+        assert file.exists
+
+
+def test_kdump_srv(host):
+    vrt = host.ansible("setup")["ansible_facts"]["ansible_virtualization_type"]
+    if vrt != 'docker':
+        srv = host.service('kdump')
+        assert srv.is_running
+        assert srv.is_enabled
